60dc38c2447a97fa83fb4623ed0be00c9042f8a8
chandeepadissanayake@gmail.com <Chandee1>
2019-07-08 23:51:31 +0530

    Deletions staged

--- welcome.h
+++
@@ -1,8 +0,0 @@
-/**
- * Author: D. M. C. Dissanayake
- * Student No: PS/2017/035
- * Date: 02/07/2019
- * Purpose: Welcome Screen of the POSKeep(Point Of Sale Application) - Header File
- **/
- 
-void poskeep_print_welcome();
